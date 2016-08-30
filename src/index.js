291c6452d528522555c59fda4bfc0d7fd9235829
João Figueiredo <lucalanca2@hotmail.com>
2016-08-31 01:20:22 +0200

    Changes browserHistory to hashHistory

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { browserHistory } from 'react-router'
+import { hashHistory } from 'react-router'
 import { syncHistoryWithStore } from 'react-router-redux'
 
 import configureStore from './configureStore'
@@ -9,7 +9,7 @@ import Root from './4.containers/root'
 import './index.css';
 
 const store = configureStore()
-const history = syncHistoryWithStore(browserHistory, store)
+const history = syncHistoryWithStore(hashHistory, store)
 render(
   <Root store={store} history={history} />,
   document.getElementById('root')
